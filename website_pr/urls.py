28eacbd6e864106eb8b525243d9dbd1fe421d967
Krzysztof Mański <krzysztof.manski@gmail.com>
2021-05-20 10:39:12 +0200

    website app

--- website_pr/urls.py
+++ website_pr/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Main.as_view(), name='main'),
 ]
